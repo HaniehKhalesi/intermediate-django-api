86bf72112f4622525e20c2a5c26726c7b03d7681
Hanieh Khalesi <khalesihanieh@gmail.com>
2023-04-20 16:43:49 +0330

    create module profiles_api

--- src/src/settings.py
+++ src/src/settings.py
@@ -73,11 +73,22 @@ WSGI_APPLICATION = "src.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'Blog_api_intermediate',
+       'USER': 'hanieh',
+       'PASSWORD': '<password>',
+       'HOST': '<database_hostname_or_ip>',
+       'PORT': '<database_port>',
+   }
 }
 
 
