a97626b0951cf2cee651bc3fd92d010701fab207
Hanieh Khalesi <khalesihanieh@gmail.com>
2023-04-23 13:38:51 +0330

    write url for api_viewSet

--- src/profiles_api/ulrs.py
+++ src/profiles_api/ulrs.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import HelloAPIView
+from .views import HelloAPIView, TestAPIViewSet
+from rest_framework import routers
 
+router = routers.SimpleRouter()
+router.register(r'api_viewset', TestAPIViewSet, basename='api_viewset')
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('test_api_view/', HelloAPIView.as_view(), name='test_api_view')
+urlpatterns += [
+    path('test_api_view/', HelloAPIView.as_view(), name='test_api_view'),
 ]
-
-
 
