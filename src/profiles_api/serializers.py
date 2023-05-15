95685fcbdd93f66ce33af3aa6a82c1107305ec0a
Hanieh Khalesi <khalesihanieh@gmail.com>
2023-05-16 02:10:09 +0330

    writing test for PublicUserAPITest

--- src/profiles_api/serializers.py
+++ src/profiles_api/serializers.py
@@ -1,33 +1,12 @@
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
-from rest_framework import generics, permissions, authentication, serializers
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
 from .models import UserProfile, profileFeedItems
+from django.utils.translation import gettext as _
 
 
 class HelloSerializer(serializers.Serializer):
     """ Serializers a name field for test api_view  """
     name = serializers.CharField(max_length=10)
-
-
-class CreateUserView(generics.CreateAPIView):
-    serializer_class = serializers.UserSerializers
-
-
-class CreateTokenView(ObtainAuthToken):
-    serializer_class = serializers.AthTokenSerializer
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
-
-class ManageUserView(generics.RetrieveUpdateAPIView):
-    """Manage the authenticated user."""
-    serializer_class = serializers.UserSerializers
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        """Retrieve and return the authenticated user."""
-        return self.request.user
-
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -59,3 +38,53 @@ class profileFeedItemsSerializer(serializers.ModelSerializer):
         extra_kwargs = {'user': {'read_only': True}}
 
 
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', 'name']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+    def create(self, validated_data):
+        return get_user_model().object.create_user(**validated_data)
+
+    # def update(self,instance, validate_data):
+    #     password = validate_data.pop('password', None)
+    #     user = super().update(instance, validate_data)
+    #     if password:
+    #         user.set_password(password)
+    #         user.save()
+    #     return user
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password,
+        )
+        if not user:
+            msg = _('unable create token ')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
