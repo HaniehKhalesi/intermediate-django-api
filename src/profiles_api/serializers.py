ba5fe8b9fc2dbb49d21fc5334a9e312a7312174b
Hanieh Khalesi <khalesihanieh@gmail.com>
2023-05-15 23:08:22 +0330

    create serializer for create user

--- src/profiles_api/serializers.py
+++ src/profiles_api/serializers.py
@@ -1,10 +1,33 @@
-from rest_framework import serializers
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework import generics, permissions, authentication, serializers
 from .models import UserProfile, profileFeedItems
 
 
 class HelloSerializer(serializers.Serializer):
     """ Serializers a name field for test api_view  """
     name = serializers.CharField(max_length=10)
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializers
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = serializers.AthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = serializers.UserSerializers
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
+
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -34,3 +57,5 @@ class profileFeedItemsSerializer(serializers.ModelSerializer):
         model = profileFeedItems
         fields = ('id', 'user', 'text_status', 'date_create')
         extra_kwargs = {'user': {'read_only': True}}
+
+
